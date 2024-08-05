5ac981ad50961d1b2076c60480ef99d8e907a9f6
hadiseh787 <hadisebagherian@gmail.com>
2024-08-05 20:33:11 +0330

    mini dore

--- mini-course.php
+++ mini-course.php
@@ -13,19 +13,63 @@ $login_url = add_query_arg('redirect_to', urlencode($current_url), $login_url);
 
 <main>
     <?php if ( is_user_logged_in() ) { ?>
-        <section class="home intro">
+        <section class="mini-course">
             <div class="container-fluid">
                 <div class="row justify-content-center">
                     <div class="col-lg-9 col-md-9 col-sm-12">
-                        <div class="box">
-                            <h2>ما مصمم هستیم مهارت فروش را در ایران ارتقا دهیم!</h2>
+                        <div class="first-section">
+                            <h2>
+                                <?php echo the_title() ?>
+                            </h2>
                             <div class="img">
-                                <img src="<?php echo SALES_ACADEMY; ?>/assets/img/banner.png" alt="">
+                                <?php the_post_thumbnail(); ?>
                             </div>
                         </div>
+                        <div class="second-section">
+                            <h3>
+                               این دوره چه کمکی به شما میکند؟
+                            </h3>
+                            <div class="text">
+                                <?php echo the_field("text"); ?>
+                            </div>
+                        </div>
+                        <div class="third-section">
+                            <h3>
+                             سر فصل های دوره
+                            </h3>
+                            <div class="text">
+                                <?php if (have_rows('repeater')): ?>
+                                    <?php while (have_rows('repeater')): the_row();
+                                        $number = get_sub_field('number');
+                                        $title = get_sub_field('title');
+                                        $iframe = get_sub_field('iframe');
+                                        ?>
+                                        <div class="video">
+
+                                                <?php echo $number ?>
+
+                                        </div>
+                                    <?php endwhile; ?>
+                                <?php endif; ?>
+                            </div>
+                        </div>
+                        <div class="fourth-section">
+                            <h3>این دوره به شما...</h3>
+                        </div>
+                        <div class="fifth-section">
+                            <h3>از چه کسانی یاد میگیرید؟</h3>
+                        </div>
+                        <div class="sixth-section">
+                            <h3>سوالات متداول</h3>
+                        </div>
+                        <div class="seventh-section">
+                            <?php comments_template(); ?>
+                        </div>
                     </div>
-                    <div class="col-lg-10 col-md-10 col-sm-12">
-                        <a href="https://didarsalesdemy.com/courses/form/">شرکت در دوره‌ها</a>
+                    <div class="col-lg-3 col-md-9 col-sm-12">
+                        <div class="detail-box">
+
+                        </div>
                     </div>
                 </div>
             </div>
